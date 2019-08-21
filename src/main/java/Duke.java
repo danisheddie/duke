b9a342eb527dc39d4f9bba37006c96599f7f9b15
danisheddie <danisheddie1405@gmail.com>
2019-08-22 01:00:36 +0800

    Level 2 done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -10,15 +11,24 @@ public class Duke {
 //        System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you");
+        ArrayList<String> lib = new ArrayList<String>();
         Scanner sc = new Scanner(System.in);
 
         while (true) {
             String cmd = sc.nextLine();
-            if (cmd.equals("bye")) {
+            if (cmd.equals("list")) {
+                for (int i = 0; i < lib.size(); i++) {
+                    System.out.print(i + 1);
+                    System.out.println(". " + lib.get(i));
+                }
+                // Display everything
+            } else if (cmd.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else {
+                lib.add(cmd);
+                System.out.println("added: " + cmd);
             }
-            System.out.println(cmd);
 
         }
     }
