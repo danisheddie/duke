1b0a8cf8517c0408aa3512db8b5fa094214dd69a
danisheddie <danisheddie1405@gmail.com>
2019-09-10 23:12:40 +0800

    [Refactor] list command

--- src/main/java/Commands/ListCmd.java
+++ src/main/java/Commands/ListCmd.java
@@ -1,8 +1,21 @@
 package Commands;
 
+import model.TaskList;
+
 public class ListCmd {
+    private TaskList taskList;
+    public ListCmd(TaskList taskList){
+        this.taskList = taskList;
+    }
 
+    public void execute(){
+        System.out.println("Here are the tasks in your list: ");
 
+        for (int i = 0; i < taskList.getTaskListSize(); i++) {
+            System.out.print(i + 1);
+            System.out.println("." + taskList.getTask(i).getDescription());
+        }
+    }
 }
 
 
