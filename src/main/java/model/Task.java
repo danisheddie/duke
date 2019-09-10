5e8f6dbb6a0d539bae0978468c6576ad4f7347f5
danisheddie <danisheddie1405@gmail.com>
2019-09-10 23:29:24 +0800

    [JavaDoc] Added documentation

--- src/main/java/model/Task.java
+++ src/main/java/model/Task.java
@@ -1,28 +1,58 @@
 package model;
 
+/**
+ * You can control the interface of how each method of the task can be handles
+ * This is a parent class for the sub-classes of Todo, Event and deadlines
+ * This class contains the method of how each sub-classes can be implemented
+ *
+ * @author Danish
+ * @version 0.1
+ * @since 2019-08-14
+ */
+
 // Task contains the behavious to getStatusicon, tickIcon, getFormat, getDescription
 public class Task {
     private String description;
     public boolean isDone;
-//    private String date = "";
 
+    /**
+     * This is the constructor to initialise description and whether the task is done
+     * @param description
+     */
     public Task(String description) { // This is a constructor
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * The status icon whether it is completed or incomplete
+     * The tick represents complete, the cross represents incomplete
+     * @return status of the task
+     */
     public String getStatusIcon() {
         return (isDone ? "✓" : "✘"); //return tick or X symbols
     }
 
+    /**
+     * To change the tatus of the task once it is done
+     * @return status of the task
+     */
     public void tickIcon() {
         isDone = true;
     }
 
+    /**
+     * This method is to get the proper format
+     * @return format of the string
+     */
     public String getFormat() {
         return "x";
     }
 
+    /**
+     * The method that will retrieve the description of the task
+     * @return Description that describes the task
+     */
     public String getDescription() {
         return description;
     }
--- src/main/java/model/TaskList.java
+++ src/main/java/model/TaskList.java
@@ -3,32 +3,60 @@ package model;
 import java.util.ArrayList;
 
 /**
+ * The TaskList class keep a state of all the added task in the @param tasksList.
  *
+ * @author Danish
+ * @version 0.1
+ * @since 2019-08-14
  */
 
 public class TaskList {
     private ArrayList<Task> taskList;
 
-    public TaskList() { // Constructor for TaskList to instantiate taskList
+    /**
+     * Constructor of the TaskList that initialize the TaskList
+     */
+    public TaskList() {
         taskList = new ArrayList<Task>();
     }
 
+    /**
+     * Method to add the task to the lisr
+     * @param task
+     */
     public void addToTask(Task task) {
         taskList.add(task);
     }
 
+    /**
+     * Method to return the task list in ArrayList type
+     * @return the list of the task
+     */
     public ArrayList<Task> getTaskList() {
         return taskList;
     }
 
+    /**
+     * Get the size of the taskList
+     * @return size of task list
+     */
     public int getTaskListSize() {
         return taskList.size();
     }
 
+    /**
+     * Gets the index of the particular task
+     * @param index
+     * @return the particular index of the task
+     */
     public Task getTask(int index) {
         return taskList.get(index);
     }
 
+    /**
+     * Deletes the task given the id of the task
+     * @param index of the id of the task
+     */
     public void deleteTaskList(int index) {
         taskList.remove(index - 1);
     }
